d648904751b2f1839d009cedeb6e448d8aaa0299
Erisvaldo Carvalho Silva <erisvaldocarvalhosilva@gmail.com>
2022-11-17 22:02:54 -0300

    Implementada a inserção do eleitor na DAL

---
+++ Urna eletrônica/SistemaEleitoral/DAL/EleitorDAL.cs
@@ -0,0 +1,29 @@
+﻿using Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class EleitorDAL
+    {
+        public void Inserir(Eleitor _eleitor)
+        {
+			SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+			SqlCommand cmd = cn.CreateCommand();
+			try
+			{
+				cmd.CommandText = "INSERT INTO Eleitor(Nome, Titutlo, Votou)VALUES(@Nome, @Titutlo, @Votou)";
+				cmd.CommandType = CommandType.Text;
+				cmd.Parameters.AddWithValue("@Nome", _eleitor.Nome);
+				cmd.Parameters.AddWithValue("@Titulo", _eleitor.Titulo);
+				cmd.Parameters.AddWithValue("@Votou", _eleitor.Votou);
+				cn.Open();
+				cmd.ExecuteNonQuery();
+			}
+			finally
+			{
+				cn.Close();
+			}
+        }
+    }
+}
