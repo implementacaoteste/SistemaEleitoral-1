1841066480ae144ff7a551652c6cea42625cb466
Erisvaldo Carvalho Silva <erisvaldocarvalhosilva@gmail.com>
2022-11-17 22:13:16 -0300

    Implementado o método Alterar na EleitorDAL

--- Urna eletrônica/SistemaEleitoral/DAL/EleitorDAL.cs
+++ Urna eletrônica/SistemaEleitoral/DAL/EleitorDAL.cs
@@ -43,5 +43,25 @@ namespace DAL
                 cn.Close();
             }
         }
+        public void Alterar(Eleitor _eleitor)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            SqlCommand cmd = cn.CreateCommand();
+            try
+            {
+                cmd.CommandText = "UPDATE Eleitor SET Nome = @Nome, Titulo = @Titulo, Votou = @Votou WHERE Id = @Id";
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@Nome", _eleitor.Nome);
+                cmd.Parameters.AddWithValue("@Titulo", _eleitor.Titulo);
+                cmd.Parameters.AddWithValue("@Votou", _eleitor.Votou);
+                cmd.Parameters.AddWithValue("@Id", _eleitor.Id);
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
     }
 }
