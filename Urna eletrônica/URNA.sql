547ccf17d2a93091c5b8cb0660d0f10c69a8cc9d
Erisvaldo Carvalho Silva <erisvaldocarvalhosilva@gmail.com>
2022-11-17 21:32:34 -0300

    Início do projeto

---
+++ Urna eletrônica/URNA.sql
@@ -0,0 +1,31 @@
+
+CREATE DATABASE Urna
+GO
+
+USE Urna
+GO
+
+CREATE TABLE Eleitor
+(
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	Nome VARCHAR(150),
+	Titulo VARCHAR(15),
+	Votou BIT
+)
+GO
+
+CREATE TABLE Candidato
+(
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	Nome VARCHAR(150),
+	Numero INT
+)
+GO
+
+CREATE TABLE Eleicao
+(
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	Ano INT,
+	Turno INT
+)
+GO
