dfb1aa0120b1c013da53dc498885b3638189e456
Erisvaldo Carvalho Silva <erisvaldocarvalhosilva@gmail.com>
2022-11-17 21:43:24 -0300

    Implementada a classe Eleitor

---
+++ Urna eletrônica/SistemaEleitoral/Models/Eleitor.cs
@@ -0,0 +1,35 @@
+﻿namespace Models
+{
+    public class Eleitor
+    {
+		private int id;
+		private string nome;
+		private string titulo;
+		private bool votou;
+
+		public bool Votou
+		{
+			get { return votou; }
+			set { votou = value; }
+		}
+
+		public string Titulo
+		{
+			get { return titulo; }
+			set { titulo = value; }
+		}
+
+		public string Nome
+		{
+			get { return nome; }
+			set { nome = value; }
+		}
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+	}
+}
